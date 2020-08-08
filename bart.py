cac0a858372cea4951efd354c2b82655cd451420
Chi-Liang <liangtaiwan1230@gmail.com>
2020-08-08 21:08:35 +0800

    easier to read model

--- bart.py
+++ bart.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 from torch import Tensor, nn
 from transformers import T5ForConditionalGeneration, BartForConditionalGeneration
+from transformers.modeling_bart import shift_tokens_right
 
 class MyBart(BartForConditionalGeneration):
     def forward(self, input_ids, attention_mask=None, encoder_outputs=None,
@@ -9,12 +10,9 @@ class MyBart(BartForConditionalGeneration):
             use_cache=False, is_training=False):
 
         if is_training:
-            decoder_start_token_id = self.config.decoder_start_token_id
-            _decoder_input_ids = decoder_input_ids.new_zeros(decoder_input_ids.shape)
-            _decoder_input_ids[..., 1:] = decoder_input_ids[..., :-1].clone()
-            _decoder_input_ids[..., 0] = decoder_start_token_id
+            _decoder_input_ids = shift_tokens_right(decoder_input_ids, self.config.pad_token_id)
         else:
-            _decoder_input_ids = decoder_input_ids.clone()
+            _decoder_input_ids = decoder_input_ids
 
         outputs = self.model(
             input_ids,
@@ -27,10 +25,9 @@ class MyBart(BartForConditionalGeneration):
         )
         lm_logits = F.linear(outputs[0], self.model.shared.weight, bias=self.final_logits_bias)
         if is_training:
-            loss_fct = nn.CrossEntropyLoss(reduce=False)
-            losses = loss_fct(lm_logits.view(-1, self.config.vocab_size),
+            loss_fct = nn.CrossEntropyLoss(reduction="sum", ignore_index=self.config.pad_token_id)
+            loss = loss_fct(lm_logits.view(-1, self.config.vocab_size),
                               decoder_input_ids.view(-1))
-            loss = torch.sum(losses * decoder_attention_mask.float().view(-1))
             return loss
         return (lm_logits, ) + outputs[1:]
 
